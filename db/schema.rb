f1196eb2efd490ab976fc701a9da56b9c771046c
Abdullah Mohamed <abdullah.abunar@gmail.com>
2020-05-30 09:01:16 +0000

    add user and email validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_29_111128) do
+ActiveRecord::Schema.define(version: 2020_05_29_114634) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.text "title"
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 2020_05_29_111128) do
     t.bigint "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
@@ -36,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_05_29_111128) do
   end
 
   add_foreign_key "comments", "articles"
+  add_foreign_key "comments", "users"
 end
