44e98e69056dcaad698b17fc8d78d2f35b4a996d
Abdullah Mohamed <abdullah.abunar@gmail.com>
2020-06-25 10:11:37 +0000

    Add tests

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriesControllerTest < ActionController::TestCase
     
     def setup
         @category = Category.create(name: "lifestyle")
+        @user = User.create(username: "john", email: "john@ex.com", password: "password", admin: true);
     end
     
     test "Should get categories index" do
@@ -12,6 +13,8 @@ class CategoriesControllerTest < ActionController::TestCase
     end
 
     test "Should get new" do
+        #sign_in_as(@user, "password")
+        session[:user_id] = @user.id
         get :new
         assert_response :success
     end
@@ -20,5 +23,12 @@ class CategoriesControllerTest < ActionController::TestCase
         #get category_path(@category)
         get :show, params: {'id': @category.id}
         assert_response :success
+    end
+
+    test "Should redirect create when not admin" do
+        assert_no_difference 'Category.count' do
+            post :create, params: { category: {name: "sports"}}
+        end
+        assert_redirected_to categories_path
     end
 end  
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+
     test "get new category form and create category" do
         get new_category_path
-        assert_template 'categories/new'
+        assert_template 'categories/new'        # needed to install a new package for this
         assert_difference 'Category.count', 1 do
             post categories_path, params: { category: {name: "sports"} }
             follow_redirect!
@@ -11,4 +12,16 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_template 'categories/index'
         assert_match "sports", response.body
     end
+
+    test "invalid category results in failure" do
+        get new_category_path
+        assert_template 'categories/new'
+        assert_no_difference 'Category.count' do
+            post categories_path, params: { category: {name: " "} }
+        end
+        assert_template 'categories/new'
+        assert_select "div.error_explanation"
+        #assert_select "div.panel-body"
+    end
+
 end
